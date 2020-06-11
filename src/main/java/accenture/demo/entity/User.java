dad70c125529e6f1ca93464c2f18ddfea7b55884
szukacs <szukacs.gergely@gmail.com>
2020-06-11 15:14:16 +0200

    ACC-03 Add user entity

---
+++ src/main/java/accenture/demo/entity/User.java
@@ -0,0 +1,28 @@
+package accenture.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String password;
+  private String cardId;
+}
