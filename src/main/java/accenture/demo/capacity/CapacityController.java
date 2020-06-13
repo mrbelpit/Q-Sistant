dec9343571e16359c6dce40b62a1751f30ca65f0
szukacs <szukacs.gergely@gmail.com>
2020-06-13 11:24:50 +0200

    ACC-09 Exit enpoint added

--- src/main/java/accenture/demo/capacity/CapacityController.java
+++ src/main/java/accenture/demo/capacity/CapacityController.java
@@ -3,6 +3,8 @@ package accenture.demo.capacity;
 import accenture.demo.user.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class CapacityController {
 
   @GetMapping("/status")
-  public ResponseEntity<?> currentStatus(@RequestBody User user){
-    return new ResponseEntity<>(CapacityHandler.currentPlaceInUserQueue(user), HttpStatus.OK);
+  public ResponseEntity<?> currentStatus(@RequestBody User user) {
+    return new ResponseEntity<>(new Message(CapacityHandler.currentPlaceInUserQueue(user)),
+        HttpStatus.OK);
+  }
+
+  @DeleteMapping("/exit")
+  public ResponseEntity<?> exitUser(@RequestBody User user) {
+    CapacityHandler.exitUser(user);
+    return new ResponseEntity<>(new Message("Exit was successful!"), HttpStatus.OK);
   }
 }
